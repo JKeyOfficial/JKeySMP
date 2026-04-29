315f1099295dab2e5fec410543781ee111de0f08
JKeyOfficial <jsecastles@gmail.com>
2026-04-29 11:28:55 +0100

    fix stripe version

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { storeItems } from "@/config/store";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2024-06-20",
+  apiVersion: "2026-04-22.dahlia" as any,
 });
 
 export async function POST(req: Request) {
--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -4,7 +4,7 @@ import { storeItems } from "@/config/store";
 import { executeRconCommand } from "@/lib/rcon";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2024-06-20",
+  apiVersion: "2026-04-22.dahlia" as any,
 });
 
 export async function POST(req: Request) {
